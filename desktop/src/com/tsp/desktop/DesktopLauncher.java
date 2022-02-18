0a7f612eedaaf67b56e892a9d18740bb77f71871
Freddy Cansick <freddycansick@gmail.com>
2022-02-18 12:34:13 +0000

    Added application icons :)

--- desktop/src/com/tsp/desktop/DesktopLauncher.java
+++ desktop/src/com/tsp/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.tsp.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
@@ -13,7 +14,11 @@ public class DesktopLauncher {
 		config.title = "Travelling Salesman";
 		config.initialBackgroundColor = Color.WHITE;
 		config.samples = 3; // anti aliasing
-		config.forceExit = false;
+		config.forceExit = true;
+		config.resizable = false;
+		config.addIcon("icon_128.png", FileType.Internal);
+		config.addIcon("icon_32.png", FileType.Internal);
+		config.addIcon("icon_16.png", FileType.Internal);
 		new LwjglApplication(new Main(), config);
 	}
 }
